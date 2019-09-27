e45cfcb7b92472dbb82c29bdcdd23566a3227911
Gustavo <g.cdcomp@gmail.com>
2019-09-27 15:44:58 -0300

    Tutorial 1

---
+++ src/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { SobrePage } from '../sobre/sobre';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  sobrePage = SobrePage;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
